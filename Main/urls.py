75ee04b1d4d5f290b8b1418aea3c3c8eff0fe0d4
radiotanemura0730 <sssryuhei0730@icloud.com>
2024-01-22 22:32:47 +0900

    プロフィールホーム画面、アカウント設定メイン完成

--- Main/urls.py
+++ Main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/<str:username>/', views.profile, name='profile'),  
+    path('home_profile/<str:username>/', views.home_profile, name='home_profile'),
+    path('settings/<str:username>/', views.settings, name='settings'),
 ]
